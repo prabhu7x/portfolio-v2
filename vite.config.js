a7461ae61a591f518e023f5ba9b1c614d4bfc16a
prabhu7x <prabhu7xyz@gmail.com>
2025-09-04 18:20:10 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prabhu.k/',
+  base: '/portfolio-v2/',
   plugins: [react()],
 });
 
